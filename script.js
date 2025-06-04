b96e4ab1d59f120b6dba10536057aab4e57e8f69
joy <djoy70986@gmail.com>
2025-06-04 09:48:29 +0600

    SecondCommit

---
+++ script.js
@@ -0,0 +1,9 @@
+function showMenuBar(){
+    const menubar = document.querySelector('.sidebar');
+    menubar.style.display ='block';
+}
+
+function hideMenubar(){
+    const menubar = document.querySelector('.sidebar');
+    menubar.style.display= 'none';
+}
